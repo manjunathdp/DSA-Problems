4dacf0199c424994e040b602dd72facf9f09253d
Manjunath Pujar <manjunathdtc3@gmail.com>
2025-04-05 20:46:25 +0530

    Check if given number is prime or not

---
+++ src/week01_Numerical/PrimeCheck.java
@@ -0,0 +1,23 @@
+package week01_Numerical;
+
+import java.util.Scanner;
+
+public class PrimeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter 'n' value: ");
+        int n = sc.nextInt();
+        boolean primeCheck=true;
+        for (int i = 2; i < n; i++) {
+            if(n%i==0) {
+                primeCheck=false;
+                break;
+            }
+        }
+        if (primeCheck) {
+            System.out.printf("%d is a Prime number",n);
+        } else {
+            System.out.printf("%d is a NOT Prime number",n);
+        }
+    }
+}
