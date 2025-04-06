883792639aee97e5d38513ada65c764b7a14401f
Manjunath Pujar <manjunathdtc3@gmail.com>
2025-04-06 19:58:22 +0530

    factorial program

---
+++ src/week01_Numerical/FactorialNumber.java
@@ -0,0 +1,16 @@
+package week01_Numerical;
+
+import java.util.Scanner;
+
+public class FactorialNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter 'n' value: ");
+        int n = sc.nextInt();
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        System.out.printf("Factorial of %d is: %d", n, fact);
+    }
+}
