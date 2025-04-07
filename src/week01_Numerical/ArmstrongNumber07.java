4de9905037244e5359d939eb11432a57bcd1f166
Manjunath Pujar <manjunathdtc3@gmail.com>
2025-04-08 00:07:59 +0530

    ArmstrongNumber

--- src/week01_Numerical/ArmstrongNumber07.java
+++ src/week01_Numerical/ArmstrongNumber07.java
@@ -7,17 +7,23 @@ public class ArmstrongNumber07 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter 'n' value: ");
         int n = sc.nextInt();
-        int digitCount=0;
+        int digitCount = 0;
         int temp = n;
-        while (temp>0){
-            temp=temp/10;
+        while (temp > 0) {
+            temp = temp / 10;
             digitCount++;
         }
-        double aStrong=0;
-        temp=n;
-        while (temp>0) {
-            int lastDigit=0;
-            aStrong=aStrong+Math.pow(lastDigit,digitCount);
+        double aStrong = 0;
+        temp = n;
+        while (temp > 0) {
+            int lastDigit = temp % 10;
+            aStrong = aStrong + Math.pow(lastDigit, digitCount);
+            temp = temp / 10;
+        }
+        if (n == aStrong) {
+            System.out.printf("%d is an Armstrong Number", n);
+        } else {
+            System.out.printf("%d is NOT an Armstrong Number", n);
         }
     }
 }
